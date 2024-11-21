5748500b12fc73ec10e60bd05cc127e95dd3168b
torutakakura <takakuratoru0@gmail.com>
2024-11-21 15:15:21 +0900

    buildエラー解消

--- app/api/todo/[todoId]/route.ts
+++ app/api/todo/[todoId]/route.ts
@@ -5,14 +5,19 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function DELETE(_: NextRequest, { params }: { params: { todoId: string } }) {
+export async function DELETE(request: NextRequest, { params }: { params: { todoId: string } }) {
   // 非同期で params を解決
-  const { todoId } = await params;
+  try {
+    const { todoId } = await params;
 
-  if (!todoId) {
-    return new NextResponse("todoId is required", { status: 400 });
+    if (!todoId) {
+      return new NextResponse("todoId is required", { status: 400 });
+    }
+
+    const res = await prisma.todo.delete({ where: { id: Number(todoId) } });
+    return NextResponse.json(res, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
-
-  const res = await prisma.todo.delete({ where: { id: Number(todoId) } });
-  return NextResponse.json(res, { status: 200 });
 }
