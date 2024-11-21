e7b28caada11e851305afc4c16f8cc5a26f39ee5
torutakakura <takakuratoru0@gmail.com>
2024-11-21 14:57:57 +0900

    軽微な修正

--- app/api/todo/[todoId]/route.ts
+++ app/api/todo/[todoId]/route.ts
@@ -3,9 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function DELETE(_: NextRequest, context: { params: { todoId: string } }) {
+export async function DELETE(_: NextRequest, { params }: { params: { todoId: string } }) {
   // 非同期で params を解決
-  const { todoId } = await context.params;
+  const { todoId } = await params;
 
   if (!todoId) {
     return new NextResponse("todoId is required", { status: 400 });
